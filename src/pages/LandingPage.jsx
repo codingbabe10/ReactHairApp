b91229b20e5a741993010d4f8a9191d2701950ad
Catrina Ryan <catrinaryan@Catrinas-MacBook-Air.local>
2024-01-25 15:53:14 -0800

    third commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import Whiteboard from "../components/Whiteboard";
+import Body from '../components/Body.jsx'
+
+export default function LandingPage() {
+    return (
+        <Body sidebar>
+            <Whiteboard />
+        </Body>
+    )
+}
